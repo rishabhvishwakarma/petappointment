5b021d565f5e447ed72f60a1d058df5c8422ac17
Rishabh <rishabh.96sharma1@gmail.com>
2021-05-22 15:16:17 +0530

    my react interface project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/index.css';
+import App from './component/App.js';
 import reportWebVitals from './reportWebVitals';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( 
+    <React.StrictMode >
+    
+    <App / >
+    
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
